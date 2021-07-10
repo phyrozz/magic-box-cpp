c485550845b5ce03f248e87740d0075b1bc8df95
phyrozz <joshuamalabanan70@gmail.com>
2021-07-10 12:54:47 +0800

    Added algorithm for singly-even squares

--- main.cpp
+++ main.cpp
@@ -4,17 +4,19 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <unistd.h>
-#include <string>
 #include <sstream>
-#include <conio.h>
 
 using namespace std;
 
+// setColor function for error texts
 void setColor(int color)
 {
  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
+// Adaptive window size depending on the size of the box
+// The maximum size of the box before the box's values starts wrapping to the next line
+// will depend on the user's screen resolution (tested on 1920x1080 monitor)
 void resizeConsoleWindow(int s)
 {
     HWND console = GetConsoleWindow();
@@ -22,9 +24,11 @@ void resizeConsoleWindow(int s)
 
     GetWindowRect(console, &ConsoleRect);
     
-    if (s > 0 && s < 9)
+    MoveWindow(console, 0, 0, s * 46, s * 50, TRUE);
+    
+    if (s > 0 && s < 15)
     {
-    	MoveWindow(console, 0, 0, 600, 600, TRUE);
+    	MoveWindow(console, 0, 0, 800, 600, TRUE);
 	}
 	else
 	{
@@ -32,13 +36,21 @@ void resizeConsoleWindow(int s)
 	}
 }
 
-void oddBox(int size)
+// Algorithm function for odd-sized box
+int** oddBox(int size)
 {	
     resizeConsoleWindow(size);
 
     int n = 1, magicConst = 0;
-    int square[size][size];
-    memset(square, 0, sizeof(square));
+    int** square = new int*[size];
+    for (int i = 0; i < size; i++)
+    {
+        square[i] = new int[size];
+        for (int j = 0; j < size; j++)
+        {
+            square[i][j] = 0;
+        }
+    }
 
     int posX = size/2;
     int posY = 0;
@@ -73,40 +85,22 @@ void oddBox(int size)
         posX++;
         posY--;
     }
-    
-    system("cls");
-    setColor(7);
 
-    cout << "\nMagic Box with size = " << size << "x" << size << "\nMagic constant = ";
-    for (int n = 0; n < size; n++)
-    {
-        magicConst += square[n][0];
-    }
-    cout << magicConst << "\n\n";
-
-    for (posY = 0; posY < size; posY++)
-    {
-        for (posX = 0; posX < size; posX++)
-        {
-            cout << setw(5) << "----";
-        }
-        cout << endl;
-        for (posX = 0; posX < size; posX++)
-        {
-            cout << "|" << setw(4) << square[posX][posY];
-        }
-        cout << "|" << endl;
-    }
-    for (posX = 0; posX < size; posX++)
-    {
-        cout << setw(5) << "----";
-    }
+    return square;
 }
 
-void doublyEven(int size)
+// Algorithm function for doubly even-sized box
+int** doublyEven(int size)
 {
 	resizeConsoleWindow(size);
-    int square[size][size], i, j, magicConst;
+
+    int** square = new int*[size];
+    int i, j, magicConst;
+
+    for (int i = 0; i < size; i++)
+    {
+        square[i] = new int[size];
+    }
 
     for (i = 0; i < size; i++)
     {
@@ -151,9 +145,126 @@ void doublyEven(int size)
         }
     }
 
-    system("cls");
-    setColor(7);
+    return square;
+}
 
+// Algorithm function for singly even-sized box
+int** singlyEven(int size)
+{
+    resizeConsoleWindow(size);
+
+    int** square = new int*[size];
+    int i, j, magicConst, swapNumbers[1][1];
+
+    int** quadrantSquare = oddBox(size/2);
+    
+    for (i = 0; i < size; i++)
+    {
+        square[i] = new int[size];
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            square[i][j] = 0;
+        }
+    }
+    
+    for (i = 0; i < size; i++)
+    {
+        if (i < size/2)
+        {
+            for (j = 0; j < size; j++)
+            {
+                if (j < size/2)
+                {
+                    square[i][j] = quadrantSquare[i][j];
+                }
+                else
+                {
+                    square[i][j] = quadrantSquare[i][j-size/2];
+                    square[i][j] = square[i][j] + size/2 * size/2 * 3;
+                }
+            }
+        }
+        else
+        {
+            for (j = 0; j < size; j++)
+            {
+                if (j < size/2)
+                {
+                    square[i][j] = quadrantSquare[i-size/2][j];
+                    square[i][j] = square[i][j] + size/2 * size/2 * 2;
+                }
+                else
+                {
+                    square[i][j] = quadrantSquare[i-size/2][j-size/2];
+                    square[i][j] = square[i][j] + size/2 * size/2;
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < (size/2)/2; i++)
+    {
+        for (j = 0; j < size/2; j++)
+        {
+            if (j == (size/2)/2)
+            {
+                swapNumbers[0][0] = square[i+1][j];
+                square[i+1][j] = square[i+1][size/2+j];
+                square[i+1][size/2+j] = swapNumbers[0][0];
+            }
+            else
+            {
+                swapNumbers[0][0] = square[i][j];
+                square[i][j] = square[i][size/2+j];
+                square[i][size/2+j] = swapNumbers[0][0];
+            }
+        }
+    }
+
+    for (i = size - 1; size-i-1 < (size/2)/2-1; i--)
+    {
+        for (j = 0; j < size/2; j++)
+        {
+            swapNumbers[0][0] = square[i][j];
+            square[i][j] = square[i][size/2+j];
+            square[i][size/2+j] = swapNumbers[0][0];
+        }
+    }
+
+    return square;
+}
+
+void printSquare(int size)
+{
+    int** square = 0;
+    if (size % 2 == 1)
+    {
+        square = oddBox(size);
+
+    }
+    else
+    {
+        if (size % 4 != 0)
+        {
+            square = singlyEven(size);
+        }
+        else
+        {
+            square = doublyEven(size);
+        }
+    }
+    
+    int magicConst = 0, x = 0, y = 0;
+    
+    system("cls");
+    // Clears the console window before printing the square
+    setColor(7);
+    
+	// Appropriate output formatting for the square
     cout << "\nMagic Box with size = " << size << "x" << size << "\nMagic constant = ";
     for (int n = 0; n < size; n++)
     {
@@ -161,27 +272,36 @@ void doublyEven(int size)
     }
     cout << magicConst << "\n\n";
 
-    for (i = 0; i < size; i++)
+    for (y = 0; y < size; y++)
     {
-        for (j = 0; j < size; j++)
+        for (x = 0; x < size; x++)
         {
             cout << setw(5) << "----";
         }
         cout << endl;
-        for (j = 0; j < size; j++)
+        for (x = 0; x < size; x++)
         {
-            cout << "|" << setw(4) << square[i][j];
+            cout << "|" << setw(4) << square[x][y];
         }
-        cout << "|" << endl;
-    }
-    for (j = 0; j < size; j++)
+            cout << "|" << endl;
+        }
+    for (x = 0; x < size; x++)
     {
         cout << setw(5) << "----";
     }
+
+	// Clean the memory allocated for the square array
+    for (int a = 0; a < size; a++)
+    {
+        delete [] square[a];
+    }
+    delete [] square;
+    square = 0;
 }
 
 void inputBoxSize(int &s)
 {
+	// Error handling
 	try
 	{
         string line;
@@ -190,7 +310,7 @@ void inputBoxSize(int &s)
         {
         	if (line == "help")
             {
-                cout << "WHAT IS A MAGIC BOX?\n" << 
+                cout << "\nWHAT IS A MAGIC BOX?\n" << 
                 "A Magic Box, or a Magic Square, is a square array of numbers, consisting of" <<
                 " distinct positive integers; and are arranged such that the sum of" <<
                 " the numbers in any horizontal, vertical, or diagonal line is always the" <<
@@ -208,10 +328,13 @@ void inputBoxSize(int &s)
                 " the same. Try it!\n\n" <<
                 "HOW DO I GENERATE A MAGIC BOX?\n" <<
                 "It's simple. Just type a number once the program asks you to enter the size" <<
-                " of the box to be generated.\nTake note that zero or negative numbers, as well" <<
+                " of the box to be generated.\nTake note that zero, two, or negative numbers, as well" <<
                 " as decimal numbers, are not allowed and will give you an error message instead.\n";
 				cout << "\nPlease enter the size of the box to be generated:\n";
 			}
+			
+			// Calling stringstream in order to provide error handling when the user inputs strings and
+			// characters.
             stringstream ss(line);
             if (ss >> s)
             {
@@ -221,22 +344,28 @@ void inputBoxSize(int &s)
                 }
             }
         }
-		if ((s % 2 == 0 && s % 4 != 0) || s < 1 || s > 47)
-		{
-			throw (s);
-		}
+        // The program can only generate squares up to 100x100. Feel free to change the number below
+        // If you desire to let the user generate bigger squares (unless you don't have a 8k monitor or
+        // something)
+        if (s < 1 || s > 100 || s == 2)
+        {
+            throw(s);
+        }
 	}
 	catch (int n)
 	{
-		if (s > 47)
+        setColor(12);
+		if (s > 100)
 		{
-            setColor(12);
-			cout << "\nThat size looks too big for a magic square. ";
+			cout << "\nThat size looks too big for a magic box. ";
 		}
+        else if (s == 2)
+        {
+            cout << "\nInvalid input. You can't make a 2x2 magic box. ";
+        }
 		else
 		{
-            setColor(12);
-			cout << "\nThe size of the box must be a positive odd integer. ";
+			cout << "\nThe size of the box must be a positive integer (except 2). ";
 		}
 		cout << "Please try again.";
         setColor(7);
@@ -247,6 +376,7 @@ void inputBoxSize(int &s)
 	}
 }
 
+// Prompt the user if they would want to generate a new box
 void inputRegenerate(int size)
 {
 	string prompt;
@@ -258,14 +388,7 @@ void inputRegenerate(int size)
 	{
 		system("cls");
 		inputBoxSize(size);
-    	if (size % 2 == 1)
-        {
-            oddBox(size);
-        }
-        else
-        {
-            doublyEven(size);
-        }
+        printSquare(size);
     	inputRegenerate(size);
 	}
 	else if (prompt == "N" || prompt == "n")
@@ -284,18 +407,13 @@ int main()
 {
     int size;
     
-    cout << "=========================== Magic Box Generator ===========================\n\n";
-    cout << "Welcome! This program can generate a magic box. Even the even ones!\n";
-    cout << "To learn more about the program and magic boxes, type \"help\".\n\n";
+    cout << "=========================== Magic Box Generator ===========================\n\n" <<
+    "Welcome! This program can generate a magic box. Even the even ones!\n" <<
+    "If you are generating larger boxes. I would recommend enabling \"Wrap Text on Resize\"\n" <<
+    "on the Console Properties.\n\n" <<
+    "To learn more about the program and magic boxes, type \"help\".\n\n";
     inputBoxSize(size);
-    if (size % 2 == 1)
-    {
-        oddBox(size);
-    }
-    else
-    {
-        doublyEven(size);
-    }
+    printSquare(size);
     inputRegenerate(size);
     
     return 0;
